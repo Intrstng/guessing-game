f358b7ab8f802648a63f07b7eb0d660d6c633c80
Intrstng <Intrstng@gmail.com>
2023-02-04 15:22:27 +0300

    init: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,34 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max) {
+        this.min = min
+        this.max = max
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return  Math.ceil((this.max - this.min) / 2 + this.min);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
 module.exports = GuessingGame;
+
+// let res = new GuessingGame
+// res.setRange(0, 100)
+// res.lower()
+// res.greater()
+// res.lower()
+// res.greater()
+// res.greater()
+// console.log(res.guess())
